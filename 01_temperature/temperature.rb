cc4e8e5c0708b2d30a209a329abbfbb6dd058862
Igor Milosavljevic <igor.milosavljevic.2644@gmail.com>
2017-12-09 19:03:31 +0100

    Completed first 3 exercises

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,8 @@
 #write your code here
+def ftoc number
+	(number - 32) * 5.0 / 9.0
+end
+
+def ctof number
+	((9.0/5.0) * number) + 32
+end
--- 02_calculator/calculator.rb
+++ 02_calculator/calculator.rb
@@ -1,1 +1,14 @@
 #write your code here
+def add number1, number2
+	number1 + number2
+end
+
+def subtract number1, number2
+	number1 - number2
+end
+
+def sum arr
+	sum = 0
+	arr.each { |x| sum += x}
+	sum
+end
