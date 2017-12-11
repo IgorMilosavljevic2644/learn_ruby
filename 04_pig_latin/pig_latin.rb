10fd5e8a63fb69a5b2c7147ae8f2843ac91b61dc
Igor Milosavljevic <igor.milosavljevic.2644@gmail.com>
2017-12-11 19:21:30 +0100

    Finished pig_latin section

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,34 @@
 #write your code here
+def translate (sent)
+
+	vowels = ["a" , "e", "i", "o", "u"]
+	consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", 
+		"n", "p", "q", "r", "s", "t", "v", "x", "z", "w", "y"]
+
+	split_sent = sent.split
+
+	split_sent.map! { |word| 
+		if first_letter_in(vowels, word)
+			word << "ay"
+		elsif first_letter_in(consonants, word)
+			point = word.index(/[aeiou]/)
+			
+			if word.include?("qu") == true
+				point = point + 1
+			end
+			
+			word = word[point..word.length] + word[0..point-1] + "ay"
+
+		end
+	}
+
+	split_sent.join(" ")
+
+end
+
+def first_letter_in (arr, sent)
+	arr.include?(sent[0]) 
+end
+
+puts translate("real glove")
+
