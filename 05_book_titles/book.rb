86f3dd2bdd63743e791b8d980a17e1c4281c1954
Igor Milosavljevic <igor.milosavljevic.2644@gmail.com>
2017-12-12 14:35:48 +0100

    6th exercise complete

--- 05_book_titles/book.rb
+++ 05_book_titles/book.rb
@@ -1,3 +1,26 @@
 class Book
 # write your code here
+	@title
+
+	attr_accessor :title
+
+	def initialize(title = "")
+		@title = title
+	end
+
+	def title
+		conjuctions = ["for", "and", "nor", "but", "or", "yet", "so"]
+		prepositions = ["in", "the", "of", "a", "an"]
+
+		split_title = @title.split
+
+		split_title.map! {|word| 
+			conjuctions.include?(word) || prepositions.include?(word) ? 
+			(word) : 
+			word.capitalize
+		}
+
+		split_title[0].capitalize!
+		split_title.join(" ")
+	end
 end
